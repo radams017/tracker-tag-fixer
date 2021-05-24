b0cc2da69297333b23b9e275fc1f40ad130d2d1f
Erik Rapert <erapert@simpli.fi>
2021-05-24 12:37:20 -0500

    added comments for clarity and refactored insertReferrerPolicy

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 
 function insertReferrerPolicy(tag) {
   var policy = "referrerpolicy='no-referrer-when-downgrade'"
-  return tag.replace(/script async/, `script async ${policy}`)
+  return tag.replace(/(script async)/, `$1 ${policy}`)
 }
 
+// tracker tags look like
+// <script async src='https://tag.simpli.fi/sifitag/b7c57f80-178c-0137-e079-06a9ed4ca31b'></script>
+// so we're finding the part like `sifitag/b7c57f80-178c-0137-e079-06a9ed4ca31b` and replacing with
+// that same part + the referer policy.
+// To be more robust we should parse out the `src` attribute into a proper URL object
+// and then add in the referer query parameter to the object and then re-stringify it.
 function insertRefererParameter(tag, ref) {
   var tagMatcher = /(sifitag\/[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12})/
   return tag.replace(tagMatcher, `$1?referer=${ref}`)
